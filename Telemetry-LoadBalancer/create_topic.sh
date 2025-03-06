296ae4fcf099d451b3503f460cf6d1339e7c9834
hongsam14 <ghdtjdgus14@gmail.com>
2025-03-06 18:09:29 +0900

    Dev: sample test successed

--- Telemetry-LoadBalancer/create_topic.sh
+++ Telemetry-LoadBalancer/create_topic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Create a topic in Kafka
-sudo docker exec -it kafka-broker00 kafka-topics --bootstrap-server localhost:9092 --create --topic otel-traces --partitions 3 --replication-factor 2 --if-not-exists
+sudo docker exec -it kafka-broker01 kafka-topics --bootstrap-server localhost:9092 --create --topic otel-traces --partitions 3 --replication-factor 2
